c34ee67a8a7a446bbeeb4acfdd4efc98962bf2d1
yuliiabalenko <yuliiabalenko@gmail.com>
2020-04-09 23:54:27 +0300

    RLE data compression

---
+++ src/task3_1/Test.java
@@ -0,0 +1,8 @@
+package task3_1;
+
+public class Test {
+    public static void main(String[] args) {
+        RunLengthEncoding rle = new RunLengthEncoding();
+        System.out.println(rle.encode("yyytYYYYYuuuu87\\IIIIIIppp"));
+    }
+}
